865b839c940049ebcca83da218e23344e10366dd
benshabbat <benshabbat27@gmail.com>
2024-01-27 22:02:06 +0200

    max created function

--- examples/max.c
+++ examples/max.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
-int main(void){
-    int myArr[]= {55,8,22,77,11,55,33,66,99,1233,321,44,7,444};
-    int max=myArr[0];
-    for(int i=1;i<=sizeof(myArr)/sizeof(myArr[0])-1;i++)
+int find_max(int *myArr, int length);
+
+int main(void)
+{
+    int myArr[] = {55, 8, 22, 77, 11, 33, 66, 99, 1233, 321, 44, 7, 444};
+    int length = sizeof(myArr) / sizeof(myArr[0]);
+    int max = find_max(myArr, length);
+    printf("%d\n", max);
+    return 0;
+}
+int find_max(int *myArr, int length)
+{
+    int max = myArr[0];
+    for (int i = 1; i <= length - 1; i++)
     {
-        if(myArr[i]>max){
-            max=myArr[i];
+        if (myArr[i] > max)
+        {
+            max = myArr[i];
         }
     }
-    printf("%d\n",max);
-    return 0;
+    return max;
 }
--- examples/min.c
+++ examples/min.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-int find_min(int *myArr);
+int find_min(int *myArr,int length);
 int main(void)
 {
     int myArr[] = {55, 8, 22, 77, 11, 55, 33, 66, 99, 1233, 321, 44, 7, 444};
-    int min = find_min(myArr);
+    int length =sizeof(myArr) / sizeof(myArr[0]);
+    int min = find_min(myArr,length);
     printf("%d\n", min);
     return 0;
 }
 
-int find_min(int *myArr)
+int find_min(int *myArr,int length)
 {
     int min = myArr[0];
-    for (int i = 1; i <= sizeof(myArr) / sizeof(myArr[0]) - 1; i++)
+    for (int i = 1; i <=length - 1; i++)
     {
         if (myArr[i] < min)
         {
