30ae82ee16dd6e73af709fdfe0003a3b6b441586
benshabbat <benshabbat27@gmail.com>
2024-01-30 17:27:59 +0200

    fix name

--- tutorial/data_types/data_type.c
+++ tutorial/data_types/data_type.c
@@ -4,7 +4,7 @@
 int main(){
 
     char a = 'C';                  // single character    %c
-    char b[] = "Bro";            // array of characters %s  
+    char b[] = "David";            // array of characters %s  
 
     float c = 3.141592;                         // 4 bytes (32 bits of precision) 6 - 7 digits %f
     double d = 3.141592653589793; // 8 bytes (64 bits of precision) 15 - 16 digits %lf
