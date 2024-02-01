cc1932ff63b44629f1310a4fc7d59a1acc08c1dd
benshabbat <benshabbat27@gmail.com>
2024-02-01 21:50:15 +0200

    tests

--- tutorial/string_functions/string_functions.c
+++ tutorial/string_functions/string_functions.c
@@ -23,7 +23,7 @@ int main()
     // int result = strcmp(string1, string2);         // string compare all characters
     //    int result = strncmp(string1, string2, 1);   // string compare n characters
     //    int result = strcmpi(string1, string2);        // string compare all (ignore case)
-    //    int result = strnicmp(string1, string2, 1);  // string compare n characters (ignore case)
+       int result = strnicmp(string1, string2, 1);  // string compare n characters (ignore case)
 
     printf("%d", result);
     //    printf("%s", string1);
