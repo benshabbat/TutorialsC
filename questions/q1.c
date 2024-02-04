034dc16bf5142c022d16a4fcf8dc99bdf56560df
benshabbat <benshabbat27@gmail.com>
2024-02-04 23:27:01 +0200

    into function

--- questions/q1.c
+++ questions/q1.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
-//Write a C program that reads three values of whole numbers 
+// Write a C program that reads three values of whole numbers
 //(which can be negative) into variables x, y, and z, and prints the distance between x + y to z
-
+int distance_between_two_numbers_to_third(int x, int y, int z)
+{
+    printf("Enter Number1#: \n");
+    scanf("%d", &x);
+    printf("Enter Number2#: \n");
+    scanf("%d", &y);
+    printf("Enter Number3#: \n");
+    scanf("%d", &z);
+    // if the num is negative i using *-1
+    printf("The distance between the two numbers(x and y) to the third number(z):%d", (x + y) - z < 0 ? ((x + y) - z) * -1 : (x + y) - z);
+}
 int main()
 {
-    int x,y,z;
+    int x, y, z;
 
-    printf("Enter Number1#: \n");
-    scanf("%d",&x);
-    printf("Enter Number2#: \n");
-    scanf("%d",&y);
-    printf("Enter Number3#: \n");
-    scanf("%d",&z);
-    // if the num is negative i using *-1
-    printf("The distance between the two numbers(x and y) to the third number(z):%d",(x+y)-z<0?((x+y)-z)*-1:(x+y)-z); 
+    distance_between_two_numbers_to_third(x, y, z);
 
     return 0;
 }
