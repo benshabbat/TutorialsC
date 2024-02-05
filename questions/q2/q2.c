3f976c04e9aad2c0b1f21c27dcc020d60412b1bf
benshabbat <benshabbat27@gmail.com>
2024-02-05 18:28:10 +0200

    // if to back for reset value

--- questions/q2/q2.c
+++ questions/q2/q2.c
@@ -15,17 +15,38 @@ int main()
 
     do
     {
-        if(a==1)printf("the number a cannot be a 1 digit\n");
-        printf("Enter a number:\n");
-        scanf("%d", &a);
-        if(b==1)printf("the number b cannot be a 1 digit\n");
-        printf("Enter b number:\n");
-        scanf("%d", &b);
-        if(c==1)printf("the number c cannot be a 1 digit\n");
-        printf("Enter c number:\n");
-        scanf("%d", &c);
+        // the variables with digit 1
+        if (a == 1)
+            printf("the number a cannot be a 1 digit\n");
+        if (b == 1)
+            printf("the number b cannot be a 1 digit\n");
+        if (c == 1)
+            printf("the number c cannot be a 1 digit\n");
+        printf("puts number without num 1\n");
+
+        if (a == 1 || a == 0)// if to back for reset value
+        {
+            printf("Enter a number:\n");
+            scanf("%d", &a);
+            if (a == 1)
+                continue; // stop to put more digits
+        }
+        if (b == 1 || b == 0)// if to back for reset value
+        {
+            printf("Enter b number:\n");
+            scanf("%d", &b);
+            if (b == 1)
+                continue; // stop to put more digits
+        }
+        if (c == 1 || c == 0)// if to back for reset value
+        {
+            printf("Enter c number:\n");
+            scanf("%d", &c);
+            if (c == 1)
+                continue; // stop to put more digits
+        }
     } while (a == 1 || b == 1 || c == 1);
 
-    printf("the res is :%d\n",((a+1) + (1+b)) % c);
+    printf("the res is :%d\n", ((a + 1) + (1 + b)) % c);
     return 0;
 }
