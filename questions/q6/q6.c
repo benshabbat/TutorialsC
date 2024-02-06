722f3e5309827482b6510b8d3058f39fe1bf85a9
benshabbat <benshabbat27@gmail.com>
2024-02-06 23:20:37 +0200

    q6 done

---
+++ questions/q6/q6.c
@@ -0,0 +1,42 @@
+/*
+Write a program in C that reads 10 numbers and stores them into an array,
+then displays them in reverse order
+*/
+
+#include <stdio.h>
+void reverse_sort(int array[], int size)
+{
+   for(int i = 0; i < size - 1; i++)
+   {
+      for(int j = 0; j < size - i - 1; j++)
+      {
+         if(array[j] < array[j+1])
+         {
+            int temp = array[j];
+            array[j] = array[j+1];
+            array[j+1] = temp;
+         }
+      }
+   }
+}
+
+void printArray(int array[], int size)
+{
+   for(int i = 0; i < size; i++)
+   {
+      printf("%d ", array[i]);
+   }
+}
+int main()
+{
+    int size = 10;
+    int array[size];
+    // int size = sizeof(array) / sizeof(array[0]);
+    for(int i = 0; i < size; i++){
+        printf("Enter the number %d# \n", i+1);
+        scanf("%d", &array[i]);
+    }
+    reverse_sort(array, size);
+    printArray(array, size);
+    return 0;
+}
