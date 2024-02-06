0d368537a3c31ed6097a69bb2eba7af3cde35ac3
benshabbat <benshabbat27@gmail.com>
2024-02-06 20:56:15 +0200

    ball animation

--- questions/q5/q5.c
+++ questions/q5/q5.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <windows.h>
 
 void gotoxy(int x, int y)
@@ -15,22 +16,57 @@ int main()
 {
     int min_x = 0;
     int max_x = 79;
+
     int symbol_racket = 178;
     int size_racket = 8;
     char symbol_ball = 'O';
-    for (int y = 0; y < size_racket; y++)
-    {
-        gotoxy(min_x, y);
-        printf("%c",symbol_racket);
-    }
-    for (int y = 0; y < size_racket; y++)
-    {
-        gotoxy(max_x, y);
-        printf("%c",symbol_racket);
-    }
 
-        gotoxy(max_x/2, size_racket/2);
-        printf("%c",symbol_ball);
+    int ball_velocity_x = 1;
+    int ball_velocity_y = 1;
+
+    int ball_coord_x = max_x / 2;
+    int ball_coord_y = size_racket / 2;
+
+    while (true)
+    {
+
+        if (ball_coord_x == 0)
+        {
+            ball_coord_x += ball_velocity_x;
+        }
+        if (ball_coord_x == max_x)
+        {
+            ball_coord_x -= ball_velocity_x;
+        }
+        if (ball_coord_y == size_racket)
+        {
+            ball_coord_y -= ball_velocity_y;
+        }
+        if (ball_coord_y == 0)
+        {
+            ball_coord_y += ball_velocity_y;
+        }
+        for (int y = 0; y < size_racket; y++)
+        {
+            gotoxy(min_x, y);
+            printf("%c", symbol_racket);
+        }
+        for (int y = 0; y < size_racket; y++)
+        {
+            gotoxy(max_x, y);
+            printf("%c", symbol_racket);
+        }
+
+        gotoxy(ball_coord_x, ball_coord_y);
+        printf("%c", symbol_ball);
+
+        Sleep(100);
+
+        gotoxy(ball_coord_x, ball_coord_y);
+        printf("%c", "");
+        ball_coord_x += ball_velocity_x;
+        ball_coord_y += ball_velocity_y;
+    }
 
     return 0;
 }
