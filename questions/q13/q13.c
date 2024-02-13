1126c4905dc28737639affe8bae20b3956d9439a
benshabbat <benshabbat27@gmail.com>
2024-02-13 18:25:23 +0200

    fix

--- questions/q13/q13.c
+++ questions/q13/q13.c
@@ -23,35 +23,26 @@ void print_student_data(Student const* to_print) {
 
 Date* date_create(int day, int month, int year)
 {
-    // Allocate memory for the Date struct
-    Date* new_date = (Date*)malloc(sizeof(Date));
-
-    // Check if memory allocation was successful
-    if (new_date == NULL) {
-        printf("Memory allocation failed.\n");
-        exit(EXIT_FAILURE);
+    Date* new_date = malloc(sizeof(Date));
+    if (new_date) {
+        new_date->day = day;
+        new_date->month = month;
+        new_date->year = year;
     }
-
-    // Initialize the Date struct members
-    new_date->day = day;
-    new_date->month = month;
-    new_date->year = year;
-
-    // Return a pointer to the created Date struct
     return new_date;
 }
 
 int date_get_day(const Date* date) {
 
-    return date->day;
+    return date ? date->day : -1;
 }
 
 int date_get_month(const Date* date) {
-    return date->month;
+    return date ? date->month : -1;
 }
 
 int date_get_year(const Date* date) {
-    return date->year;
+    return date ? date->year : -1;
 }
 
 void date_free(Date* to_free)
@@ -61,37 +52,28 @@ void date_free(Date* to_free)
 
 Time* time_create(int hour, int minute, int second)
 {
-    // Allocate memory for the Date struct
-    Time* new_time = (Time*)malloc(sizeof(Time));
-
-    // Check if memory allocation was successful
-    if (new_time == NULL) {
-        printf("Memory allocation failed.\n");
-        exit(EXIT_FAILURE);
+    Time* new_time = malloc(sizeof(Date));
+    if (new_time) {
+        new_time->hour = hour;
+        new_time->minute = minute;
+        new_time->second = second;
     }
-
-    // Initialize the Time struct members
-    new_time->hour = hour;
-    new_time->minute = minute;
-    new_time->second = second;
-
-    // Return a pointer to the created Time struct
     return new_time;
 }
 
 int time_get_hour(const Time* time) {
     // TODO: Implement this function
-    return time->hour;
+    return time ? time->hour : -1;
 }
 
 int time_get_minute(const Time* time) {
     // TODO: Implement this function
-    return time->minute;
+    return time ? time->minute : -1;
 }
 
 int time_get_second(const Time* time) {
     // TODO: Implement this function
-    return time->second;
+    return time ? time->second : -1;
 }
 
 void time_free(Time* to_free)
@@ -101,45 +83,41 @@ void time_free(Time* to_free)
 
 DateTime* datetime_create(Date* date, Time* time)
 {
-    // Allocate memory for the DateTime struct
-    DateTime* new_datetime = (DateTime*)malloc(sizeof(DateTime));
-
-    if (new_datetime == NULL) {
-        // Handle memory allocation failure
-        return NULL;
+    DateTime* new_datetime = malloc(sizeof(DateTime));
+    if (new_datetime) {
+        new_datetime->date = date;
+        new_datetime->time = time;
     }
-
-    // Assign the provided Date and Time structs to the DateTime struct members
-    new_datetime->date = date;
-    new_datetime->time = time;
-
-    // Return a pointer to the created DateTime struct
     return new_datetime;
 }
 
 const Date* datetime_get_date(const DateTime* dateTime) {
 
-    return &(dateTime->date);
+    return dateTime ? dateTime->date : NULL;
 }
 
 const Time* datetime_get_time(const DateTime* dateTime) {
     // TODO: Implement this function
-    return &(dateTime->time);
+    return dateTime ? dateTime->time : NULL;
 }
 
 void datetime_free(DateTime* to_free)
 {
-    free(to_free);
+    if (to_free) {
+        date_free(to_free->date);
+        time_free(to_free->time);
+        free(to_free);
+    }
 }
 
 const char* student_get_name(const Student* student) {
     // TODO: Implement this function
-    return student->name;
+    return student ? student->name : NULL;
 }
 
 const DateTime* student_get_birth_date(const Student* student) {
     // TODO: Implement this function
-    return &(student->birth_date);
+    return student ? student->birth_date : NULL;
 }
 
 const Track student_get_track(const Student* student) {
@@ -149,34 +127,30 @@ const Track student_get_track(const Student* student) {
 
 uint8_t student_get_height(const Student* student) {
     // TODO: Implement this function
-    return student->height;
+    return student ? student->height : 0;
 }
 
 const uint8_t* student_get_id(const Student* student) {
     // TODO: Implement this function
-    return student->id;
+    return student ? student->id : NULL;
 }
 
 Student* student_create(DateTime* birthDate, char* name, Track track, uint8_t height, uint8_t* id) {
-    // Allocate memory for the DateTime struct
-    Student* new_student = (DateTime*)malloc(sizeof(DateTime));
-
-    // Check if memory allocation was successful
-    if (new_student == NULL) {
-        printf("Memory allocation failed.\n");
-        exit(EXIT_FAILURE);
+    Student* new_student = malloc(sizeof(Student));
+    if (new_student) {
+        new_student->birth_date = birthDate;
+        new_student->name = name; 
+        new_student->track = track;
+        new_student->height = height;
+        memcpy(new_student->id, id, 9 * sizeof(uint8_t));
     }
-    // Assign the provided Date and Time pointers to the DateTime struct members
-    new_student->birth_date = birthDate;
-    new_student->name = name;
-    new_student->track = track;
-    new_student->height = height;
-    new_student->id = id;
-
-    // Return a pointer to the created DateTime struct
     return new_student;
 }
 
 void student_free(Student* to_free) {
-    free(to_free);
+    if (to_free) {
+        datetime_free(to_free->birth_date);
+        free(to_free->name);
+        free(to_free);
+    }
 }
