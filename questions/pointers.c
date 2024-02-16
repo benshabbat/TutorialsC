0ada9ada426474d29b1a740e8b3c4f4688b7abbd
benshabbat <benshabbat27@gmail.com>
2024-02-16 12:20:59 +0200

    // ex 2

--- questions/pointers.c
+++ questions/pointers.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 int main()
 {
-    int x = 5;
-    int* p_x = &x;
-    int** p_p_x = &p_x;
+    // int x = 5;
+    // int* p_x = &x;
+    // int** p_p_x = &p_x;
+    // // p_x+=1;
+    // printf("pointer %p p_p %p",p_x,p_p_x );
+
+    // ex 2
+    int x[5];
+
+    //*(x+1) == x[1];
+    x[1] = 56;
+    *(x + 1) = 44;
+
     // p_x+=1;
-    printf("pointer %p p_p %p",p_x,p_p_x );
+    printf("*(x+1) %d", x[1]);
+
     return 0;
 }
