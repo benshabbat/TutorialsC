b8719697b8969a87955cad14de4dbe9ec34ecf3c
benshabbat <benshabbat27@gmail.com>
2024-02-17 22:23:37 +0200

    ex4

--- questions/pointers.c
+++ questions/pointers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main()
 {
     // int x = 5;
@@ -17,15 +18,31 @@ int main()
     // printf("*(x+1) %d", x[1]);
 
     // ex 3
-    int x[5];
+    // int x[5];
 
     //*(x+1) == x[1];
     // x[1] = 56;
     // *(x + 1) = 44;
     //&(*(x+1))==&X[1]
-    printf("Enter a number");
-    scanf("%d", &(*(x+1)));
-    printf("*(x+1) %d", x[1]);
+    // printf("Enter a number");
+    // scanf("%d", &(*(x+1)));
+    // printf("*(x+1) %d", x[1]);
+
+
+    //ex4
+    int num =0;
+    printf("Enter a number ");
+    scanf("%d", &num);
+    int* nums = (int*)malloc(num * sizeof(int));
+
+    for(int i=0; i<num; i++){
+        printf("Enter a numbers ");
+        scanf("%d", nums+i);  // nums+i ==> &nums[i];
+    }
+    for(int i=0; i<num; i++){
+        printf("%d\n", *(nums+i)); // *(nums+i)) ==> nums[i];
+    }
+    free(nums);
 
     return 0;
 }
