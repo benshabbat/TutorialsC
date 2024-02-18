eec787866571fe04f83063d5cbaf3fe64d48d46c
benshabbat <benshabbat27@gmail.com>
2024-02-18 13:26:23 +0200

    memchr

--- memory_manipulation.c
+++ memory_manipulation.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-    int arr1[2]={1,2};
-    int arr2[2]={3,4};
+int main()
+{
+    int arr1[2] = {1, 2};
+    int arr2[2] = {3, 4};
 
     // memset(arr1,1,sizeof(arr1));
     // printf("%d %d\n",arr1[0],arr1[1]);
 
-
+    if (memchr(arr1, 2, sizeof(arr1)) != NULL)
+    {
+        printf("the number into arr1");
+    }
+    else
+        printf("the number dosent into arr1");
 }
