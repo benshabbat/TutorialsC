c9aba1357d98dbf76e08a343a4c0e008cce6cc3e
benshabbat <benshabbat27@gmail.com>
2024-02-18 13:23:40 +0200

    memset

---
+++ memory_manipulation.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int arr1[2]={1,2};
+    int arr2[2]={3,4};
+
+    // memset(arr1,1,sizeof(arr1));
+    // printf("%d %d\n",arr1[0],arr1[1]);
+
+
+}
