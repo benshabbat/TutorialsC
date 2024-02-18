4622a1df2ec6aa388f7977ae1c5a6087952c3e7e
benshabbat <benshabbat27@gmail.com>
2024-02-18 13:30:34 +0200

    memcmp

--- memory_manipulation.c
+++ memory_manipulation.c
@@ -9,14 +9,24 @@ int main()
     // memset(arr1,1,sizeof(arr1));
     // printf("%d %d\n",arr1[0],arr1[1]);
 
-    if (memchr(arr1, 2, sizeof(arr1)) != NULL)
+    // if (memchr(arr1, 2, sizeof(arr1)) != NULL)
+    // {
+    //     printf("the number into arr1");
+    //     printf(memchr(arr1, 2, sizeof(arr1)));
+    // }
+    // else
+    // {
+    //     memchr(arr1, 3, sizeof(arr1));
+    //     printf("the number dosent into arr1");
+    // }
+
+
+    if (memcmp(arr1, arr2, sizeof(int)*2) ==0)
     {
-        printf("the number into arr1");
-        printf(memchr(arr1, 2, sizeof(arr1)));
+        printf("the arrays is equal");
     }
     else
     {
-        memchr(arr1, 3, sizeof(arr1));
-        printf("the number dosent into arr1");
+        printf("they are not equal");
     }
 }
