f05b16850259bf4e0558f5184385d96dff3d6b9d
benshabbat <benshabbat27@gmail.com>
2024-02-25 23:29:55 +0200

    user struct

--- onlineBanking/onlineBanking.c
+++ onlineBanking/onlineBanking.c
@@ -2,6 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 
+struct User{
+    char phone[100];
+    char account[100];
+    char password[100];
+    float balance;
+}
+
 int main()
 {
     return 0;
