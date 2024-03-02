4f95f885cdbf516d7272e768dc3552ee083e33e0
benshabbat <benshabbat27@gmail.com>
2024-03-02 22:20:10 +0200

    try 1

--- questions/quick_sort/quick_sort.c
+++ questions/quick_sort/quick_sort.c
@@ -1,6 +1,9 @@
 #include "Question.h"
-void swap(int *x, int *y){
-    
+void swap(int *x, int *y)
+{
+    int temp =*x;
+    *x = *y;
+    *y = temp;
 }
 // Function to partition the array and return the pivot index
 int partition(int arr[], int low, int high) {
@@ -9,8 +12,17 @@ int partition(int arr[], int low, int high) {
 
     // Complete the implementation here:
     // START
-
+    for (int j = low; j< high; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            swap(&arr[i],&arr[j]);
+            i++;
+        }
+    }
+    swap(&arr[i],&arr[high]);
     // END
+    return i;
 }
 
 // Function to implement QuickSort
@@ -21,7 +33,8 @@ void quickSort(int arr[], int low, int high) {
 
         // Complete the implementation here:
         // START
-
+        quickSort(arr, low, pi-1);
+        quickSort(arr, pi+1, high);
         // END
     }
 }
