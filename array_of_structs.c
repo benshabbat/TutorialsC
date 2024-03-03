2cafe3cf0864d08b7684a09e82ed267b997f231e
benshabbat <benshabbat27@gmail.com>
2024-03-03 23:29:51 +0200

    char of pinter into struct

--- array_of_structs.c
+++ array_of_structs.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct
 {
     int x;
     int y;
+    char *desc;
 } Point;
 
 int main()
@@ -24,23 +26,30 @@ int main()
     {
         points[i].x = 100 * i;
         points[i].y = 20 * i;
+        points[i].desc = malloc(sizeof(char) * 10);
+        strcpy(points[i].desc, "Point");
     }
     for (int i = 0; i < size; i++)
     {
         printf("x=%d\n", points[i].x);
         printf("y=%d\n", points[i].y);
+        printf("Point %d=%s\n",i+1, points[i].desc);
     }
-    printf("\n\n after realloc");
+    printf("\n\n after realloc\n\n");
     points = realloc(points, sizeof(Point) * (size * 2));
     for (int i = 0; i < size * 2; i++)
     {
         points[i].x = 100 * i;
         points[i].y = 20 * i;
+        points[i].desc = malloc(sizeof(char) * 10);
+        strcpy(points[i].desc, "Point");
     }
-    for (int i = 0; i < size*2; i++)
+    for (int i = 0; i < size * 2; i++)
     {
         printf("x=%d\n", points[i].x);
         printf("y=%d\n", points[i].y);
+        printf("Point %d=%s\n",i+1, points[i].desc);
+        free(points[i].desc);
     }
 
     free(points);
