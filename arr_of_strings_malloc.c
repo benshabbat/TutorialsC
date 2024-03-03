bb10e8b67300f20a429c500394a8a84e61ab24ad
benshabbat <benshabbat27@gmail.com>
2024-03-03 18:15:37 +0200

    strdup

---
+++ arr_of_strings_malloc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+
+    char **strings;
+    int total;
+
+    printf("Enter a number total of Strings");
+    scanf("%d", &total);
+    strings = malloc(total*sizeof(char*));
+    for(int i = 0; i < total; i++){
+        strings[i] = strdup(strings[i]);
+        
+    }
+
+    return 0;
+}
