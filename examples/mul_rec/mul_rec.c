724b2280e01685fd2f290a68f5d7d54a51224ab3
benshabbat <benshabbat27@gmail.com>
2024-03-04 21:12:41 +0200

    nicer

--- examples/mul_rec/mul_rec.c
+++ examples/mul_rec/mul_rec.c
@@ -7,13 +7,14 @@ float swap(float *a, float *b){
 
 float mul_rec(float x, float y){
     if(x==0||y==0) return 0;
-    if(y<0)swap(&x,&y);
-    if(x<0&&y<0) {y*=-1; x*=-1;}
+    // if(y<0)swap(&x,&y);
+    if(y<0)return mul_rec(x,y+1)-x;
+    // if(x<0&&y<0) {y*=-1; x*=-1;}
     return mul_rec(x,y-1)+x;
 }
 
 int main(){
-    int num1=-5;
+    int num1=5;
     int num2=-5;
     
     printf("%lf\n",mul_rec(num1,num2));
