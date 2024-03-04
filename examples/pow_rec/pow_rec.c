12b62f8a1126051953de23b1d598492edc2da66b
benshabbat <benshabbat27@gmail.com>
2024-03-04 21:22:52 +0200

    better

--- examples/pow_rec/pow_rec.c
+++ examples/pow_rec/pow_rec.c
@@ -4,16 +4,18 @@ float pow_rec(float x, int n)
 {
     if (n == 1)
         return x;
-    else if(n<0)
+    else if (n < 0)
     {
-        n*=-1;
-        x=1/x;
+        // n*=-1;
+        // x=1/x;
+        return pow_rec(x, n + 1) * 1 / x;
     }
-    else if(n==0)return 1;
+    else if (n == 0)
+        return 1;
     return pow_rec(x, n - 1) * x;
 }
 int main()
 {
-    printf("%lf\n", pow_rec(2, -2));
+    printf("%lf\n", pow_rec(2, -3));
     return 0;
 }
