7af7b5081051cb3c0a0ae74aabcf9cf3f600ee55
benshabbat <benshabbat27@gmail.com>
2024-03-06 18:25:55 +0200

    made example for it rn i need to imprve it

--- leetcode/two_sum/two_sum.c
+++ leetcode/two_sum/two_sum.c
@@ -1,6 +1,24 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
+#include <stdio.h>
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize);
+
+int main() {
+    int nums[] = {2, 7, 11, 15};
+    int target = 9;
+    int returnSize;
+    int* result = twoSum(nums, 4, target, &returnSize);
+
+    if (result != NULL) {
+        printf("Indices: %d, %d\n", result[0], result[1]);
+        free(result);
+    } else {
+        printf("No solution found.\n");
+    }
+
+    return 0;
+}
+
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int *returnArr = malloc(2*sizeof(int));
     for(int i=0;i<numsSize;i++){
