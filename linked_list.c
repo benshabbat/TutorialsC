fcf169a74ad160e7fb5a91d4d5e5852c28abb98d
benshabbat <benshabbat27@gmail.com>
2024-03-10 20:32:34 +0200

    add node

--- linked_list.c
+++ linked_list.c
@@ -22,15 +22,30 @@ Node *create_Node(int value)
 
 void add_Node(Node **head, Node *node)
 {
-    
+    if (*head == NULL)
+    {
+        *head = node;
+        return;
+    }
+
+    node->next = *head;
+    *head = node;
 }
 
 int main()
 {
     Node *head = NULL;
     Node *new_node = create_Node(4);
+    Node *new_node2 = create_Node(6);
 
     printf("%d\n", new_node->value);
+    printf("%d\n", new_node2->value);
+    add_Node(&head, new_node);
+    add_Node(&head, new_node2);
 
+    // Print the value of the first node
+    printf("%d\n", head->value);
+    // Free the memory allocated for the nodes
+    free(new_node);
     return 0;
 }
