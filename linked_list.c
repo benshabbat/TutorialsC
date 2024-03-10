646116e15a900a1e120f6ae33a53e04692d5ab30
benshabbat <benshabbat27@gmail.com>
2024-03-10 20:26:04 +0200

    better

--- linked_list.c
+++ linked_list.c
@@ -7,22 +7,30 @@ typedef struct
     struct Node *next;
 } Node;
 
-Node *create_Node(Node *node, int value)
+Node *create_Node(int value)
 {
-    node = malloc(sizeof(Node));
-    node->value = value;
-    node->next = NULL;
-    return node;
+    Node *new_node = (Node *)malloc(sizeof(Node));
+    if (new_node == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+    new_node->value = value;
+    new_node->next = NULL;
+    return new_node;
 }
 
 void add_Node(Node **head, Node *node)
 {
+    
 }
 
 int main()
 {
     Node *head = NULL;
-    Node *new_node = create_Node(new_node, 4);
+    Node *new_node = create_Node(4);
+
     printf("%d\n", new_node->value);
+
     return 0;
 }
