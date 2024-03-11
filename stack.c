06696e407812cff28994095de2581866d51b5423
benshabbat <benshabbat27@gmail.com>
2024-03-12 00:08:37 +0200

    making some bases of functions

--- stack.c
+++ stack.c
@@ -8,10 +8,28 @@ typedef struct{
     int size;
 }Stack;
 
+Stack *create_stack(int capacity){
+
+}
+void destroy_stack(Stack *stack){
+
+}
+
+bool is_empty(Stack *stack){
+
+}
+
+bool is_full(Stack *stack){
+
+}
+
 void push(int value) {
     
 }
 
+void pop() {
+
+}
 int main(){
     return 0;
 }
