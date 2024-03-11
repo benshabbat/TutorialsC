bcf63b6852f1410a5cf0e35b3bf255b2279d2bf3
benshabbat <benshabbat27@gmail.com>
2024-03-12 00:04:49 +0200

    making base for stack

--- stack.c
+++ stack.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct{
     int *array;
+    int capacity;
     int size;
 }Stack;
+
+void push(int value) {
+    
+}
 
 int main(){
     return 0;
