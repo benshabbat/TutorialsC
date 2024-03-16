e65ebf80e2400e799c83fec764c977b8a5787e98
benshabbat <benshabbat27@gmail.com>
2024-03-17 00:46:58 +0200

    q1 done

--- queue/q2.c
+++ queue/q2.c
@@ -1,7 +1,8 @@
 #include "Question.h"
 #include <stdlib.h>
 
-#define MAX_SIZE 100 // keep it 100
+
+#define MAX_SIZE 100
 
 struct Queue {
     int front, rear;
@@ -9,53 +10,43 @@ struct Queue {
 };
 
 // Function to initialize an empty queue
-struct Queue* createQueue() {
-    struct Queue* queue = (struct Queue*)malloc(sizeof(struct Queue));
+struct Queue *createQueue() {
+    struct Queue *queue = (struct Queue *) malloc(sizeof(struct Queue));
     queue->front = -1;
     queue->rear = -1;
     return queue;
 }
 
 // Function to check if the queue is empty
-int isEmpty(struct Queue* queue) {
-    // Complete the implementation here:
-    // START
+int isEmpty(struct Queue *queue) {
     return queue->front == -1;
-    // END
 }
 
 // Function to check if the queue is full
-int isFull(struct Queue* queue) {
-    // Complete the implementation here:
-    // START
+int isFull(struct Queue *queue) {
     return (queue->rear + 1) % MAX_SIZE == queue->front;
-
-    // END
 }
 
 // Function to enqueue an element to the queue
-void enqueue(struct Queue* queue, int item) {
+void enqueue(struct Queue *queue, int item) {
     if (isFull(queue)) {
         return;
     }
-    // Complete the implementation here:
-    // START
+
     if (isEmpty(queue)) {
         queue->front = 0;
     }
 
     queue->rear = (queue->rear + 1) % MAX_SIZE;
     queue->array[queue->rear] = item;
-    // END
 }
 
 // Function to dequeue an element from the queue
-int dequeue(struct Queue* queue) {
+int dequeue(struct Queue *queue) {
     if (isEmpty(queue)) {
         return -1; // Assuming -1 is not a valid element in the queue
     }
-    // Complete the implementation here:
-    // START
+
     int item = queue->array[queue->front];
     if (queue->front == queue->rear) {
         // Reset the queue to empty state after the last element is dequeued
@@ -66,16 +57,19 @@ int dequeue(struct Queue* queue) {
     }
 
     return item;
-    // END
 }
 
 // Function to get the front element of the queue without dequeueing
-int front(struct Queue* queue) {
+int front(struct Queue *queue) {
     if (isEmpty(queue)) {
         return -1; // Assuming -1 is not a valid element in the queue
     }
+    return queue->array[queue->front];
+}
+
+int firstUniqueCharacter(const char *s) {
     // Complete the implementation here:
     // START
-    return queue->array[queue->front];
+
     // END
 }
