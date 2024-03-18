f99e0f45f309bc8f5036b4f30e1323feb47e8b94
benshabbat <benshabbat27@gmail.com>
2024-03-18 22:02:42 +0200

    prittier

--- linked-list/q4.c
+++ linked-list/q4.c
@@ -3,15 +3,18 @@
 #include <stdlib.h>
 
 // Node structure
-struct Node {
+struct Node
+{
     int data;
-    struct Node* next;
+    struct Node *next;
 };
 
 // Function to create a new node
-struct Node* createNode(int data, struct Node* next) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    if (newNode == NULL) {
+struct Node *createNode(int data, struct Node *next)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    if (newNode == NULL)
+    {
         exit(EXIT_FAILURE);
     }
 
@@ -21,35 +24,41 @@ struct Node* createNode(int data, struct Node* next) {
     return newNode;
 }
 
-int getData(struct Node* node) {
-    if(node == NULL)
+int getData(struct Node *node)
+{
+    if (node == NULL)
         return -1;
     return node->data;
 }
 
-struct Node* getNext(struct Node* node) {
-    if(node == NULL)
+struct Node *getNext(struct Node *node)
+{
+    if (node == NULL)
         return NULL;
     return node->next;
 }
 
 // Function to insert a new node at the beginning of the linked list
-struct Node* insertAtBeginning(struct Node* head, int data) {
-    struct Node* newNode = createNode(data, NULL);
+struct Node *insertAtBeginning(struct Node *head, int data)
+{
+    struct Node *newNode = createNode(data, NULL);
     newNode->next = head;
     return newNode;
 }
 
 // Function to insert a new node at the end of the linked list
-struct Node* insertAtEnd(struct Node* head, int data) {
-    struct Node* newNode = createNode(data, NULL);
+struct Node *insertAtEnd(struct Node *head, int data)
+{
+    struct Node *newNode = createNode(data, NULL);
 
-    if (head == NULL) {
+    if (head == NULL)
+    {
         return newNode;
     }
 
-    struct Node* current = head;
-    while (current->next != NULL) {
+    struct Node *current = head;
+    while (current->next != NULL)
+    {
         current = current->next;
     }
 
@@ -57,18 +66,21 @@ struct Node* insertAtEnd(struct Node* head, int data) {
     return head;
 }
 
-struct Node* oddEvenList(struct Node* head) {
+struct Node *oddEvenList(struct Node *head)
+{
     // Complete the implementation here:
     // START
-    if (head == NULL || head->next == NULL) {
+    if (head == NULL || head->next == NULL)
+    {
         return head; // If the list is empty or has only one node, no need to rearrange
     }
 
-    struct Node* odd = head;
-    struct Node* even = head->next;
-    struct Node* evenStart = even; // Save the start of even list
+    struct Node *odd = head;
+    struct Node *even = head->next;
+    struct Node *evenStart = even; // Save the start of even list
 
-    while (even != NULL && even->next != NULL) {
+    while (even != NULL && even->next != NULL)
+    {
         odd->next = even->next;
         odd = odd->next;
         even->next = odd->next;
