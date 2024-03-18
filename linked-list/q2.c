7e9edc0527f87d4fa62b22516d252c55116e78b1
benshabbat <benshabbat27@gmail.com>
2024-03-18 19:45:52 +0200

    middleNode

---
+++ linked-list/q2.c
@@ -0,0 +1,78 @@
+#include "Question.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data, struct Node* next) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    newNode->data = data;
+    newNode->next = next;
+
+    return newNode;
+}
+
+int getData(struct Node* node) {
+    if(node == NULL)
+        return -1;
+    return node->data;
+}
+
+struct Node* getNext(struct Node* node) {
+    if(node == NULL)
+        return NULL;
+    return node->next;
+}
+
+// Function to insert a new node at the beginning of the linked list
+struct Node* insertAtBeginning(struct Node* head, int data) {
+    struct Node* newNode = createNode(data, NULL);
+    newNode->next = head;
+    return newNode;
+}
+
+// Function to insert a new node at the end of the linked list
+struct Node* insertAtEnd(struct Node* head, int data) {
+    struct Node* newNode = createNode(data, NULL);
+
+    if (head == NULL) {
+        return newNode;
+    }
+
+    struct Node* current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    current->next = newNode;
+    return head;
+}
+
+struct Node *middleNode(struct Node *head)
+{
+    // Complete the implementation here:
+    // START
+    if (head == NULL) {
+        return NULL;
+    }
+    struct Node *fast = head;
+    struct Node *slow = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+    
+
+    // END
+}
