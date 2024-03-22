eca334ffcd1544c7c82633e79bd36e2405b7330b
benshabbat <benshabbat27@gmail.com>
2024-03-22 14:31:28 +0200

    not pq but working

--- priority-queue/q3.c
+++ priority-queue/q3.c
@@ -95,5 +95,22 @@ int top(struct PriorityQueue *pq) {
 }
 
 int maxProduct(int *nums, int numsSize) {
-
+    int max1 = nums[0], max2 = nums[1];
+    
+    if (max1 < max2) {
+        int temp = max1;
+        max1 = max2;
+        max2 = temp;
+    }
+    
+    for (int i = 2; i < numsSize; i++) {
+        if (nums[i] >= max1) {
+            max2 = max1;
+            max1 = nums[i];
+        } else if (nums[i] > max2) {
+            max2 = nums[i];
+        }
+    }
+    
+    return (max1 - 1) * (max2 - 1);
 }
