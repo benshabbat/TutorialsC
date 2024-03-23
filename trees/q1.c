9d772241e4cdccc2281c31a43620888df23cf337
benshabbat <benshabbat27@gmail.com>
2024-03-23 23:09:51 +0200

    getNumOfNodes start

---
+++ trees/q1.c
@@ -0,0 +1,84 @@
+#include "Question.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define max(a, b) ((a > b) ? (a) : (b))
+
+// Node structure for a binary tree
+struct TreeNode {
+    int data;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+// Function to create a new node
+struct TreeNode* createNode(int data) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    if (newNode == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+// Function to get data of node
+int getData(struct TreeNode* node) {
+    if(node == NULL)
+        return -1;
+    return node->data;
+}
+
+// Function to insert leftNode for node
+struct TreeNode* insertLeftNode(struct TreeNode* node, int data) {
+    if (node == NULL)
+        return NULL;
+    struct TreeNode* left = createNode(data);
+    node->left = left;
+    return left;
+}
+
+// Function to insert rightNode for node
+struct TreeNode* insertRightNode(struct TreeNode* node, int data) {
+    if (node == NULL)
+        return NULL;
+    struct TreeNode* right = createNode(data);
+    node->right = right;
+    return right;
+}
+
+// Function to get number of node in tree rooted in root
+int getNumOfNodes(struct TreeNode* root) {
+    if(root == NULL)
+        return 0;
+
+    // Complete the implementation here:
+    // START
+
+    // END
+}
+
+// Function to get number of leaves in tree rooted in root
+int getNumOfLeaves(struct TreeNode* root) {
+    if(root == NULL)
+        return 0;
+
+    // Complete the implementation here:
+    // START
+
+    // END
+}
+
+// Function to get depth of the tree rooted in root
+int getDepth(struct TreeNode* root) {
+    if(root == NULL)
+        return 0;
+
+    // Complete the implementation here:
+    // START
+
+    // END
+}
