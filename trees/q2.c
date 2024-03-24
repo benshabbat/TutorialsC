a973e3ff99029b1742c632cfa1e4334a28f3abeb
benshabbat <benshabbat27@gmail.com>
2024-03-24 02:04:26 +0200

    preOrderTraversal

--- trees/q2.c
+++ trees/q2.c
@@ -5,16 +5,19 @@
 #include <ctype.h>
 
 // Node structure for a binary tree
-struct TreeNode {
+struct TreeNode
+{
     int data;
-    struct TreeNode* left;
-    struct TreeNode* right;
+    struct TreeNode *left;
+    struct TreeNode *right;
 };
 
 // Function to create a new node
-struct TreeNode* createNode(int data) {
-    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
-    if (newNode == NULL) {
+struct TreeNode *createNode(int data)
+{
+    struct TreeNode *newNode = (struct TreeNode *)malloc(sizeof(struct TreeNode));
+    if (newNode == NULL)
+    {
         exit(EXIT_FAILURE);
     }
 
@@ -26,40 +29,55 @@ struct TreeNode* createNode(int data) {
 }
 
 // Function to get data of node
-int getData(struct TreeNode* node) {
-    if(node == NULL)
+int getData(struct TreeNode *node)
+{
+    if (node == NULL)
         return -1;
     return node->data;
 }
 
 // Function to insert leftNode for node
-struct TreeNode* insertLeftNode(struct TreeNode* node, int data) {
+struct TreeNode *insertLeftNode(struct TreeNode *node, int data)
+{
     if (node == NULL)
         return NULL;
-    struct TreeNode* left = createNode(data);
+    struct TreeNode *left = createNode(data);
     node->left = left;
     return left;
 }
 
 // Function to insert rightNode for node
-struct TreeNode* insertRightNode(struct TreeNode* node, int data) {
+struct TreeNode *insertRightNode(struct TreeNode *node, int data)
+{
     if (node == NULL)
         return NULL;
-    struct TreeNode* right = createNode(data);
+    struct TreeNode *right = createNode(data);
     node->right = right;
     return right;
 }
 
 // Function to traverse the tree in preorder
-void preOrderTraversal(struct TreeNode* root, int* result, int* index){
+void preOrderTraversal(struct TreeNode *root, int *result, int *index)
+{
     // Complete the implementation here:
     // START
-    
+    if (root == NULL)
+        return;
+
+    // Visit the root
+    result[(*index)++] = root->data;
+
+    // Traverse the left subtree
+    preOrderTraversal(root->left, result, index);
+
+    // Traverse the right subtree
+    preOrderTraversal(root->right, result, index);
     // END
 }
 
 // Function to traverse the tree in inorder
-void inOrderTraversal(struct TreeNode* root, int* result, int* index) {
+void inOrderTraversal(struct TreeNode *root, int *result, int *index)
+{
     // Complete the implementation here:
     // START
 
@@ -67,7 +85,8 @@ void inOrderTraversal(struct TreeNode* root, int* result, int* index) {
 }
 
 // Function to traverse the tree in postorder
-void postOrderTraversal(struct TreeNode* root, int* result, int* index) {
+void postOrderTraversal(struct TreeNode *root, int *result, int *index)
+{
     // Complete the implementation here:
     // START
 
