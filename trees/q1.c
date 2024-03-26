8ab6d3a3881ef03aef1ccb5e371377bc0af71e74
benshabbat <benshabbat27@gmail.com>
2024-03-26 18:28:20 +0200

    maxDepth

--- trees/q1.c
+++ trees/q1.c
@@ -5,19 +5,16 @@
 #define max(a, b) ((a > b) ? (a) : (b))
 
 // Node structure for a binary tree
-struct TreeNode
-{
+struct TreeNode {
     int data;
-    struct TreeNode *left;
-    struct TreeNode *right;
+    struct TreeNode* left;
+    struct TreeNode* right;
 };
 
 // Function to create a new node
-struct TreeNode *createNode(int data)
-{
-    struct TreeNode *newNode = (struct TreeNode *)malloc(sizeof(struct TreeNode));
-    if (newNode == NULL)
-    {
+struct TreeNode* createNode(int data) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    if (newNode == NULL) {
         exit(EXIT_FAILURE);
     }
 
@@ -29,71 +26,62 @@ struct TreeNode *createNode(int data)
 }
 
 // Function to get data of node
-int getData(struct TreeNode *node)
-{
-    if (node == NULL)
+int getData(struct TreeNode* node) {
+    if(node == NULL)
         return -1;
     return node->data;
 }
 
 // Function to insert leftNode for node
-struct TreeNode *insertLeftNode(struct TreeNode *node, int data)
-{
+struct TreeNode* insertLeftNode(struct TreeNode* node, int data) {
     if (node == NULL)
         return NULL;
-    struct TreeNode *left = createNode(data);
+    struct TreeNode* left = createNode(data);
     node->left = left;
     return left;
 }
 
 // Function to insert rightNode for node
-struct TreeNode *insertRightNode(struct TreeNode *node, int data)
-{
+struct TreeNode* insertRightNode(struct TreeNode* node, int data) {
     if (node == NULL)
         return NULL;
-    struct TreeNode *right = createNode(data);
+    struct TreeNode* right = createNode(data);
     node->right = right;
     return right;
 }
 
 // Function to get number of node in tree rooted in root
-int getNumOfNodes(struct TreeNode *root)
-{
-    if (root == NULL)
+int getNumOfNodes(struct TreeNode* root) {
+    if(root == NULL)
         return 0;
 
     // Complete the implementation here:
     // START
+    if(root == NULL)
+        return 0;
     else
         return 1 + getNumOfNodes(root->left) + getNumOfNodes(root->right);
     // END
 }
 
 // Function to get number of leaves in tree rooted in root
-int getNumOfLeaves(struct TreeNode *root)
-{
-    if (root == NULL)
+int getNumOfLeaves(struct TreeNode* root) {
+    if(root == NULL)
         return 0;
 
     // Complete the implementation here:
     // START
-    else if (root->left == NULL && root->right == NULL)
-        return 1;
-    else
-        return getNumOfLeaves(root->left) + getNumOfLeaves(root->right);
+
     // END
 }
 
 // Function to get depth of the tree rooted in root
-int getDepth(struct TreeNode *root)
-{
-    if (root == NULL)
-        return -1;
+int getDepth(struct TreeNode* root) {
+    if(root == NULL)
+        return 0;
 
     // Complete the implementation here:
     // START
-    else {
-        return max(getDepth(root->left), getDepth(root->right))+1;
-    }
+
     // END
 }
