1b552219f87fcd5ed5471047b94f2fd63b42a601
benshabbat <benshabbat27@gmail.com>
2024-03-28 00:44:22 +0200

    reverseList

---
+++ leetcode/206. Reverse Linked List/file.c
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode *prev = NULL, *current = head, *next = NULL;
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
+}
