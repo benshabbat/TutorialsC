f86195b93371e171ac50e1e656f3a12127d1477c
benshabbat <benshabbat27@gmail.com>
2024-04-06 22:43:36 +0300

    create_node

--- doubly_node.c
+++ doubly_node.c
@@ -6,3 +6,10 @@ typedef struct Node{
     struct Node* next;
     struct Node* prev;
 }Node;
+
+Node* create_node(int value){
+    Node* new_node = malloc(sizeof(Node));
+    new_node->value = value;
+    new_node->next = NULL;
+    new_node->prev = NULL;
+}
